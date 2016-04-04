cc33b87554f040bd8ff5b64a912f87a5e2426a49
srhbnnstr <srhbnnstr@gmail.com>
2016-04-03 22:38:20 -0700

    updated profile, seed.js, server.js and index.html

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
-                  "mongodb://localhost/personal-api");
+// mongoose.connect( process.env.MONGOLAB_URI ||
+//                   process.env.MONGOHQ_URL ||
+//                   "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
 module.exports.videoGames = require("./videoGames.js");
